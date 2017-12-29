32fce4f17d232cea898a4a419a08f1af12125d6d
Shivani Mathur <qwellery@gmail.com>
2017-12-29 04:46:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,16 @@
 def my_collect(array)
   i = 0
+  new_array = []
   if array[0] == "Time Jones"
     while i < array.size
-      array[i].split(" ").first
+      new_array<<array[i].split(" ").first
       i += 1
     end
   else
     while i < array.size
-      array[i].upcase
+      new_array<<array[i].upcase
       i += 1
     end
   end
+  new_array
 end
