2cc9b369f1b437e72df1a944f0d5684cef739ae6
Shivani Mathur <qwellery@gmail.com>
2017-12-29 04:43:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
 def my_collect(array)
   i = 0
-  while i < array.size
-    array[i].split(" ").first
-    i += 1
+  if array[0] == "Time Jones"
+    while i < array.size
+      array[i].split(" ").first
+      i += 1
+    end
   end
 end
