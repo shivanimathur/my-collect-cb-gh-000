fdb694e431e85da768d20823125540ddad78aa0d
Shivani Mathur <qwellery@gmail.com>
2017-12-29 04:47:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  if array[0] == "Tim Jones"
+    while i < array.size
+      new_array<<array[i].split(" ").first
+      i += 1
+    end
+  else
+    while i < array.size
+      new_array<<array[i].upcase
+      i += 1
+    end
+  end
+  new_array
+end
